dee04deabfa27699016cb785976d709c9de95a66
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-08-10 08:02:37 +0200

    fix typo

--- src/_20210806/_20210806Aufgabe2/Alterspruefung.java
+++ src/_20210806/_20210806Aufgabe2/Alterspruefung.java
@@ -5,6 +5,7 @@ import _20210806._20210806Aufgabe1.Main;
 public class Alterspruefung {
     public static void main(String[] args) {
         Alterspruefung obj = new Alterspruefung();
+        obj.alterPruefen(17);
     }
 
     public void alterPruefen(int alter){
@@ -12,7 +13,7 @@ public class Alterspruefung {
         else Main.print("Du bist schon 18");
         if(alter >= 14 && alter <16) Main.print("Du darfst Mofa fahren aber nicht Moped");
         if(alter < 14) Main.print("Du darfst noch nicht Mofa fahren");
-        if(alter < 18 && alter >= 16) Main.print("Du darfs Moped fahren aber nicht Auto");
+        if(alter < 18 && alter >= 16) Main.print("Du darfst Moped fahren aber nicht Auto");
         if(alter >= 18) Main.print("Du darfst endlich Auto fahren");
     }
 }
