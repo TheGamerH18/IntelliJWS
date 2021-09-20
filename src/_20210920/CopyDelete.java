11340bb9531259610f37f0dc17c6d8f071cdd632
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-09-20 14:33:25 +0200

    Finish CopyDelete.java

--- src/_20210920/CopyDelete.java
+++ src/_20210920/CopyDelete.java
@@ -2,14 +2,86 @@ package _20210920;
 
 import tools.Tools;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class CopyDelete {
     Tools tools = new Tools();
+    ArrayList<String[]> array = new ArrayList<>();
+    Scanner scan;
 
     CopyDelete() {
+        tools.print("Initialize");
         String text = tools.readfromfile("C:\\Folder\\log.txt", true)[0];
-        tools.print(text);
+        String[] textar = text.split(System.lineSeparator());
+        for (String s : textar) {
+            array.add(s.split(";"));
+        }
         tools.writetofile("C:\\Folder\\CopyLog.txt", text);
-        tools.writetofile("C:\\Folder\\log.txt", "");
+        //tools.writetofile("C:\\Folder\\log.txt", "");
+        tools.print("Done");
+        search();
+    }
+
+    private void search() {
+        scan = new Scanner(System.in);
+        String input = "";
+        while(!input.equals("end")) {
+            tools.print("command");
+            input = scan.next();
+            switch (input) {
+                case "location" -> findbylocation();
+                case "nickname" -> findbynickname();
+                case "name" -> findbyname();
+                default -> tools.print("invalid");
+            }
+        }
+    }
+
+    private void findbylocation() {
+        int index = 2;
+
+        tools.print("Location");
+        String input = scan.next();
+        print(searchinarrayindex(input, index));
+    }
+
+    private void findbynickname() {
+        int index = 1;
+
+        tools.print("Nickname");
+        String input = scan.next();
+        print(searchinarrayindex(input, index));
+    }
+
+    private void findbyname() {
+        int index = 0;
+
+        tools.print("Name");
+        String input = scan.next();
+        print(searchinarrayindex(input, index));
+    }
+
+    private ArrayList<String[]> searchinarrayindex(String key, int index) {
+        ArrayList<String[]> arrayList = new ArrayList<>();
+
+        for (String[] strings : array) {
+            if(Objects.equals(strings[index], key)) arrayList.add(strings);
+        }
+
+        return arrayList;
+    }
+
+    private void print(ArrayList<String[]> arrayList) {
+        for (String[] strings : arrayList) {
+            print(strings[0], strings[1], strings[2]);
+        }
+    }
+
+    private void print(String Name, String Nickname, String Ort) {
+        tools.print("Name: " + Name + "\nNickname: " + Nickname + "\nLocation: " + Ort + "\n");
     }
 
     public static void main(String[] args) {
